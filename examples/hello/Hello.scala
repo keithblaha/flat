1ae037e49bd9ad129abd69dfcb735190fbfd901e
Keith Blaha <keith.blaha@gmail.com>
2016-08-20 22:12:44 -0700

    better ordering in hello example

--- examples/hello/Hello.scala
+++ examples/hello/Hello.scala
@@ -9,16 +9,16 @@ object Hello extends App with FlatApp {
     OK("hello!")
   }
 
-  app.route("/can-you-handle-anything") { request =>
-    OK("you bet")
-  }
-
   app.route("/flat-is-neat", List(GET, POST)) { request =>
     OK("i know ;)")
   }
 
   app.route("/flat-is-lame", List(GET, POST)) { request =>
     BadRequest("""¯\_(ツ)_/¯""")
+  }
+
+  app.route("/can-you-handle-anything") { request =>
+    OK("you bet")
   }
 
   app.start
